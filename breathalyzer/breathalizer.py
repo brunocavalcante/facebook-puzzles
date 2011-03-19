c6e4e8d3727a0e1232d3df66c43852fa31ca0ad1
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-03-19 15:21:47 -0300

    Comments

--- breathalyzer/breathalizer.py
+++ breathalyzer/breathalizer.py
@@ -2,41 +2,45 @@
 import sys
 
 def number_of_changes(word1, word2):
-	if word1 == word2:
-		return 0 
+    "Calculates the number of changes from word1 to word2"
+    if word1 == word2:
+        return 0 
         
-	length_diff = abs(len(word1) - len(word2))
+    length_diff = abs(len(word1) - len(word2))
 
-	changes = 0
-	removals = 0
-	for i in range(0, len(word1)):
-		j = 0 if (i - removals == 0) else i - removals
+    changes = 0
+    removals = 0
+    for i in range(0, len(word1)):
+        j = 0 if (i - removals == 0) else i - removals
 
-		if j < len(word2) and word1[i] != word2[j]:
-		    if len(word1) <= len(word2) or removals >= length_diff:
-		        changes = changes + 1
-		    else:
-		        j = i + removals + 1
-		        if j < len(word1) and i < len(word2) and word1[j] == word2[i]:
-		            removals = removals + 1
-		        else:
-		            changes = changes + 1
+        if j < len(word2) and word1[i] != word2[j]:
+            if len(word1) <= len(word2) or removals >= length_diff:
+                changes = changes + 1
+            else:
+                j = i + removals + 1
+                if j < len(word1) and i < len(word2) and word1[j] == word2[i]:
+                    removals = removals + 1
+                else:
+                    changes = changes + 1
 
-	return changes + length_diff
+    return changes + length_diff
 
+# Wall Post Words
 wall_post = open(sys.argv[1], 'r')
-wall_post_words = map(lambda l: l.rstrip().split(" "), wall_post.readlines())
+wall_post_words = [l.rstrip().split(" ") for l in wall_post.readlines()]
 
+# List of Accepted Words
 accepted_words_file = open('twl06.txt', 'r')
-accepted_words = map(lambda l: l.rstrip(), accepted_words_file.readlines())
+accepted_words = [l.rstrip() for l in accepted_words_file.readlines()]
 
+# Calculate the smallest ammount of changes necessary
 total_of_changes = 0
 for word in wall_post_words[0]:
     word_changes = None
     for accepted_word in accepted_words:
-		changes = number_of_changes(word.upper(), accepted_word)
-		if word_changes is None or changes < word_changes:
-			word_changes = changes
+        changes = number_of_changes(word.upper(), accepted_word)
+        if word_changes is None or changes < word_changes:
+            word_changes = changes
 
     total_of_changes = total_of_changes + word_changes
 
