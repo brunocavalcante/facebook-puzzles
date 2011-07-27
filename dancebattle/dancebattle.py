dcecb2e50241012133b391a2a62bd8b1257e9afe
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-07-27 12:32:58 -0300

    Changes to dance battle

--- dancebattle/dancebattle.py
+++ dancebattle/dancebattle.py
@@ -1,14 +1,19 @@
 #dancebattle.py
-import sys, re
+import sys, re, itertools
 
 data = open(sys.argv[1], 'r')
 total_of_moves = int(data.readline())
 total_of_turns = int(data.readline())
 
+total_of_possible_turns = 0
+for c in itertools.combinations_with_replacement(range(total_of_moves), 2):
+    total_of_possible_turns += 1 
+
+print "Total de combinacoes possiveis", total_of_possible_turns
+
 winner = False 
 used_turns = []
 used_moves = []
-i = 1
 try:
     for line in data.readlines():
         turn = re.split('\s*', line.rstrip())
@@ -23,13 +28,11 @@ try:
             if move not in used_moves:
                 used_moves.append(move) 
         used_turns.append(turn)
-        i += 1
-except:
-    if i % 2 == 0:
-        winner = True
 
-if not winner:
-    if (i + total_of_moves - len(used_moves)) % 2 == 0:
+    if (len(used_turns) + total_of_moves - len(used_moves)) % 2 == 0:
+        winner = True
+except:
+    if len(used_turns) % 2 == 0:
         winner = True
 
 if winner:
