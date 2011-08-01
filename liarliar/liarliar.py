1d09e3b92aa0998d81792fab13b6a7035266b4be
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-08-01 14:09:45 -0300

    Correção do liarliar

--- liarliar/liarliar.py
+++ liarliar/liarliar.py
@@ -1,23 +1,24 @@
 #liarliar.py
 import sys
 
-data = open('data.txt', 'r')
+data = open(sys.argv[1], 'r')
 liars = {}
 accuser = None
 for line in data:
-	columns = line.rstrip().split('   ')
-	if (len(columns) == 2):
-		accuser = columns[0]
-		if (not(accuser in liars)):
-			liars[accuser] = "false"
-	elif (not(columns[0].isdigit())):
-		person = columns[0]
-		if (accuser and liars[accuser] == "false"):
-			liars[person] = "true"
-		else:
-			liars[person] = "false"
+    columns = line.rstrip().split('   ')
+    if (len(columns) == 2):
+        accuser = columns[0]
+	if (not(accuser in liars)):
+            liars[accuser] = "false"
+    elif (not(columns[0].isdigit())):
+	person = columns[0]
+	if (accuser and liars[accuser] == "false"):
+	    liars[person] = "true"
 	else:
-		accuser = None
+	    liars[person] = "false"
+    else:
+	accuser = None
 
-number_of_liars = liars.values().count("true")
-print number_of_liars, len(liars)-number_of_liars
+total_liars = liars.values().count("true")
+total_nonliars = abs(len(liars) - total_liars)
+print max(total_liars, total_nonliars), min(total_liars, total_nonliars)
